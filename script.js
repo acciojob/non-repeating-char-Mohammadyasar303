89232ab0524c9114461a98d282a6a5f04dc39c90
Mohammadyasar Mansuri <126454357+Mohammadyasar303@users.noreply.github.com>
2025-02-06 13:13:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
-	let charFrequency = {};
-	  for(let i=0; i<str.length; i++){
-		  
+  let charFrequency = {};
+  for(let i=0; i<str.length; i++){
     let char = str[i];
     if(charFrequency[char]){
       charFrequency[char]++;
@@ -10,6 +8,14 @@ function firstNonRepeatedChar(str) {
       charFrequency[char] = 1;
     }
   }
+
+  for(let i=0; i<str.length; i++){
+    if(charFrequency[str[i]] == 1){
+      return str[i];
+    }
+  }
+
+  return null; 
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
